2550bd578c72ccbce32ad779ebc597f08bdce072
MartinSalamon <martin.salamon4@gmail.com>
2021-10-19 18:45:40 +0200

    dodajanje datotek

---
+++ zagovor/naroci-zdaj.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="header-footer/header-footer.css" />
+	<title>Spuži Natakar</title>
+</head>
+<body>
+	<?php include "header-footer/header-footer.php";?>
+
+</body>
+</html>
