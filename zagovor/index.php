9631020e44a4bd0cb5b3e034cde3f542a12dc682
MartinSalamon <martin.salamon4@gmail.com>
2021-10-20 07:45:54 +0200

    Added containers to index.php

--- zagovor/index.php
+++ zagovor/index.php
@@ -7,6 +7,14 @@
 </head>
 <body>
 	<?php include "header-footer/header.php";?>
+	
+	<div id="container">
+
+    <div id="content">
+
+    </div>
+
+    </div>
 
 	<?php include "header-footer/footer.php";?>
 </body>
