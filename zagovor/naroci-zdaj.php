e26be20f4034763a218c37c8db6cf3556d22a1fa
MartinSalamon <martin.salamon4@gmail.com>
2021-10-20 19:55:47 +0200

    A bit of progress

--- zagovor/naroci-zdaj.php
+++ zagovor/naroci-zdaj.php
@@ -3,17 +3,22 @@
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="header-footer/header-footer.css" />
+	<link rel="stylesheet" type="text/css" href="css/naroci-zdaj.css" />
 	<title>Spuži Natakar</title>
 </head>
 <body>
 	<?php include "header-footer/header.php";?>
 
 	<div id="container">
-
-    <div id="content">
-
-    </div>
-
+	    <div id="content">
+	    	<form class = "narocilo" id = "narocilo" method="post">
+	    		<label for="email" class = "label">Vaš elektronski naslov</label><br>
+	  			<input type="email" id="email" name="email" class = "input-field"><br>
+	  			<label for="email" class = "label">Naročilo</label><br>
+	  			<textarea rows="3" class = "input-field" placeholder="vnesite narocilo tukaj"></textarea><br><br>
+	  			<button type="submit" id = "oddaj-narocilo" method="post">Oddaj naročilo</button>
+	    	</form>
+	    </div>
     </div>
 
 	<?php include "header-footer/footer.php";?>
